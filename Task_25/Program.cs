2a0c061b561f9ae181bcfb855a685eda5219e4f8
Tamara <tomritol@gmail.com>
2022-05-26 23:54:11 +0300

    add task 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B. Задачу решить без использования Math.Pow.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число А - ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B - ");
+int numberB = int.Parse(Console.ReadLine());
+
+int numExponent = GetExponent (numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {numExponent}");
+
+int GetExponent (int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+
+        result = result * numA;
+    return result;
+}
